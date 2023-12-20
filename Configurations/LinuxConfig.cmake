1fddcc0b0ef82406d5ab7c19a6d0f2082fd613f5
Marat Shchuchinsky <maratshch@gmail.com>
2023-12-20 18:19:51 +0200

    modify CMake - target fine tuning

--- Configurations/LinuxConfig.cmake
+++ Configurations/LinuxConfig.cmake
@@ -4,4 +4,9 @@ set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+add_compile_options(
+	"$<$<CONFIG:DEBUG>:-O0 -mavx2>"		# disable optimization, AVX2 instruction set
+	"$<$<CONFIG:RELEASE>:-O3 -mavx2>"	# highest optimization, AVX2 instruction set
+)
+
 include(GNUInstallDirs)
--- Configurations/WindowsConfig.cmake
+++ Configurations/WindowsConfig.cmake
@@ -1,14 +1,20 @@
 # Configuration file for build LutLibrary for Windows OS (Win64 on i64 platform only supported)
 
-set(-DCMAKE_TOOLCHAIN_FILE=64bit.toolchain)
+set(CMAKE_GENERATOR_PLATFORM x64)
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+add_definitions(-D_LARGEFILE64_SOURCE=1)
+	
 if(MSVC)
 # Microsoft Visual Studio compiler used
     add_definitions(-D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS)
-	add_definitions(-D_LARGEFILE64_SOURCE=1)
+
+	add_compile_options(
+		"$<$<CONFIG:DEBUG>:/Od;/Ot>"		# disable optimization, favor fast code
+		"$<$<CONFIG:RELEASE>:/O2;/Oi;/Ot>"	# high optimization, inline functions, favor fast code
+	)
 endif(MSVC)
 
 # Intel compiler used
