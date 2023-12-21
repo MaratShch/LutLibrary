11e5e480fee931e85bee19a111f2348e258f0809
Marat Shchuchinsky <maratshch@gmail.com>
2023-12-21 23:02:26 +0200

    Cosmetic change in CMake files for fix installation path

--- Configurations/WindowsConfig.cmake
+++ Configurations/WindowsConfig.cmake
@@ -6,14 +6,31 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_definitions(-D_LARGEFILE64_SOURCE=1)
+
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 	
+set(CMAKE_INSTALL_LIB_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib)
+set(CMAKE_INSTALL_HXX_DIRECTORY ${CMAKE_INSTALL_PREFIX}/include)
+set(CMAKE_INSTALL_BIN_DIRECTORY ${CMAKE_INSTALL_PREFIX}/bin)
+set(CMAKE_INSTALL_TST_DIRECTORY ${CMAKE_INSTALL_PREFIX}/test)
+
+message (STATUS "CMAKE_INSTALL_PREFIX =  ${CMAKE_INSTALL_PREFIX}")
+message (STATUS "Install LIB folder:     ${CMAKE_INSTALL_LIB_DIRECTORY}")
+message (STATUS "Install HEADRES folder: ${CMAKE_INSTALL_HXX_DIRECTORY}")
+message (STATUS "Install BIN folder:     ${CMAKE_INSTALL_BIN_DIRECTORY}")
+message (STATUS "Install TESTS folder:   ${CMAKE_INSTALL_TST_DIRECTORY}")
+
+
 if(MSVC)
+include(InstallRequiredSystemLibraries)
 # Microsoft Visual Studio compiler used
     add_definitions(-D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS)
 
 	add_compile_options(
-		"$<$<CONFIG:DEBUG>:/Od;/Ot>"		# disable optimization, favor fast code
-		"$<$<CONFIG:RELEASE>:/O2;/Oi;/Ot>"	# high optimization, inline functions, favor fast code
+		"$<$<CONFIG:DEBUG>:/Od;/Ot;/arch:AVX2>"			# disable optimization, favor fast code, AVX2 instruction set
+		"$<$<CONFIG:RELEASE>:/O2;/Oi;/Ot;/arch:AVX2>"	# high optimization, inline functions, favor fast code, AVX2 instruction set
 	)
 endif(MSVC)
 
--- WinBuild.cmd
+++ WinBuild.cmd
@@ -1,12 +1,11 @@
-@echo 'Run CMake generatiob for Windows...'
-rmdir /S /Q Build
-md Build
-cd Build
+@echo 'Run CMake generation for Windows...'
+rmdir /S /Q build
+rmdir /S /Q install
+md build
+cd build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64 -DLUT_LIB_TESTS=ON
-cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX:PATH=..\install .. && cmake --build . --target install --config Release
 cd ..
-
-
 
 
 
