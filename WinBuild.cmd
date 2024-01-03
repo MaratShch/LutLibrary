3de4b03a60a03bd71e65bb234fdd7dd7ce708382
Marat Shchuchinsky <maratshch@gmail.com>
2024-01-03 13:36:28 +0200

    comsetic changes in WinBuild.cmd file

--- WinBuild.cmd
+++ WinBuild.cmd
@@ -1,11 +1,18 @@
 @echo 'Run CMake generation for Windows...'
-rmdir /S /Q build
-rmdir /S /Q install
-md build
-cd build
+@echo OFF
+
+set BUILD_FOLDER=build
+set INSTALL_FOLDER=install
+
+REM Remove previously created BUILD folder
+IF EXIST %BUILD_FOLDER% @rmdir /S /Q %BUILD_FOLDER%
+
+REM Remove previously created INSTALL folder
+IF EXIST %INSTALL_FOLDER% @rmdir /S /Q %INSTALL_FOLDER%
+
+md %BUILD_FOLDER%
+cd %BUILD_FOLDER%
+
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64 -DLUT_LIB_TESTS=ON
 cmake -DCMAKE_INSTALL_PREFIX:PATH=..\install .. && cmake --build . --target install --config Release
 cd ..
-
-
-
