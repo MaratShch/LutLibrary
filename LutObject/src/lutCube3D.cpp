bf39407ef427e1dbb54717777ee20be51f6c3c9f
Marat Shchuchinsky <maratshch@gmail.com>
2024-01-05 22:13:48 +0200

    add simple test (non completed yet) for debug parser for 3D CUBE LUT file. Gtest not using yet

--- LutObject/src/lutCube3D.cpp
+++ LutObject/src/lutCube3D.cpp
@@ -1,14 +1,1 @@
 #include "lutCube3D.h"
-
-void tst (void)
-{
-	CCubeLut3D<float>  fLut3D;
-	CCubeLut3D<double> dLut3D;
-	CCubeLut3D<long double> ldLut3D;
-
-	fLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
-	dLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
-	ldLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
-
-	return;
-}
--- LutObject/src/lutElement.cpp
+++ LutObject/src/lutElement.cpp
@@ -1,14 +1,1 @@
 #include "lutElement.h"
-
-void test0_lib_entry (void)
-{
-	LutElement::lutTableRaw<float> lutRaw;
-	return;
-}
-
-void test2_lib_entry (void)
-{
-	LutElement::lutTable3D<float> lut3D;
-	return;
-}
-
