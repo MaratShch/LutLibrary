d2f99825a063225c32bd0215296159fa903f0128
Marat Shchuchinsky <maratshch@gmail.com>
2024-01-05 13:18:58 +0200

    add CSP and CLF LUT's for debug and tests purposes only. These LUT's found in internet and seems are not required license for usage

--- LutObject/src/lutCube3D.cpp
+++ LutObject/src/lutCube3D.cpp
@@ -4,8 +4,11 @@ void tst (void)
 {
 	CCubeLut3D<float>  fLut3D;
 	CCubeLut3D<double> dLut3D;
+	CCubeLut3D<long double> ldLut3D;
 
 	fLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
+	dLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
+	ldLut3D.LoadCubeFile("C:\\TEMP\\lut.cube");
 
 	return;
 }
