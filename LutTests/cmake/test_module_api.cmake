070926a2c2a9a291c95070977c12aa8c69cb0b5a
MaratGMATCI <Marat.Shchuchinsky@gm.com>
2024-01-14 09:29:43 -0500

    add first and empty gtest. TODO: add real tests

---
+++ LutTests/cmake/test_module_api.cmake
@@ -0,0 +1,27 @@
+cmake_minimum_required (VERSION 3.25)
+
+
+function (lutlib_test name source libraries)
+ 
+ set (GTest_NAME ${name}.gtest)
+ set (GTest_PrivateLibraries gtest gtest_main)
+
+ add_executable (
+	${GTest_NAME}
+        ${source}
+ )
+ 
+ target_include_directories (
+	${GTest_NAME}
+	BEFORE PRIVATE ${GTest_PrivateIncludes}
+	BEFORE PUBLIC  ${GTest_Includes}
+ )
+
+ target_link_libraries (
+	${GTest_NAME}
+        PRIVATE ${GTest_PrivateLibraries}
+        PUBLIC  ${libraries}
+ )
+
+
+endfunction(lutlib_test)
