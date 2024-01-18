e836bbf7860fc52d90b478810c57c5932154c8e7
MaratGMATCI <Marat.Shchuchinsky@gm.com>
2024-01-18 06:49:54 -0500

    rename cmake configuration folder

---
+++ LutUtils/include/string_view.h
@@ -0,0 +1,19 @@
+#ifndef LUTUTILS_STRING_VIEW_PORTING
+#define LUTUTILS_STRING_VIEW_PORTING
+
+
+namespace lututils_std
+{ 
+
+  template <typename CharT, typename Traits = std::char_traits<CharT>>
+  class basic_string_view final
+  {
+    public:
+
+  } /* basic_string_view */
+
+} /* namespace lututils_std */
+
+
+
+#endif /* LUTUTILS_STRING_VIEW_PORTING */
