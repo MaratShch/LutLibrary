bac0ed9216a6ad4063f7eb5d37cfdf64b0430d30
MaratGMATCI <Marat.Shchuchinsky@gm.com>
2024-01-25 09:14:58 -0500

    add missing API to string_view class, such: iterators and compare

--- LutUtils/include/string_view.h
+++ LutUtils/include/string_view.h
@@ -36,7 +36,7 @@ namespace lututils_std
       using reverse_iterator = std::reverse_iterator<iterator>;
       using const_reverse_iterator = std::reverse_iterator<const_iterator>;
     
-      static constexpr size_type npos = static_cast<size_type>(-1);
+      static constexpr size_type npos {static_cast<size_type>(-1)};
       
       /* default constructor */
       constexpr basic_string_view () noexcept : m_string {nullptr}, m_size{0} {};
@@ -54,8 +54,9 @@ namespace lututils_std
       template <typename tAllocator>
       basic_string_view (const std::basic_string<CharT,Traits,tAllocator>& std_str) noexcept : m_string{std_str.c_str()}, m_size{std_str.size()} {};
       
-      /* assignment / copy operators */
-      basic_string_view& operator=(const basic_string_view& view) = default;
+      /* assignment / copy / move operators */
+      basic_string_view& operator=(const basic_string_view& other) = default;
+      basic_string_view& operator=(basic_string_view&& other) noexcept {swap(other); return *this;};
       
       /* capacity, size and size validation */
       constexpr size_type size()     const noexcept {return m_size;}
@@ -69,7 +70,7 @@ namespace lututils_std
       constexpr const_reference at (const size_type position) const
       {
           return (position < m_size) ? m_string[position] : 
-         (throw std::out_of_range{"Position is out of range in basic_string_view::at"}), m_string[position];
+         (throw std::out_of_range{"Position index is out of range in basic_string_view::at()"}), m_string[position];
       }
       constexpr const_reference front () const noexcept {return *m_string;}
       constexpr const_reference back  () const noexcept {return m_string[m_size - static_cast<size_type>(1)];}
@@ -79,17 +80,57 @@ namespace lututils_std
       void remove_suffix (size_type suffix_pos)     noexcept {m_size -= suffix_pos;}
       void swap          (basic_string_view& other) noexcept {std::swap(m_string, other.m_string), std::swap(m_size, other.m_size);}
       
-      /* compare */
+      /* sub-strings */
+      basic_string_view substr (size_t position = 0, size_t length = npos) const
+      {
+        auto const max_length {position > m_size ? 0 : m_size - position};
+        if (position > size()) {throw std::out_of_range("Position index is out of range in basic_string_view::substr()");}
+        return basic_string_view (m_string + position, std::min(length, max_length));
+      }
+
+      /* compare API's */
       int compare (basic_string_view s) const noexcept
       {
-          const size_type rlen = std::min(m_size, s.m_size);
-          const int compare = Traits::compare (m_string, s.m_string, rlen);
+          auto const rlen = std::min(m_size, s.m_size);
+          auto const compare = Traits::compare (m_string, s.m_string, rlen);
           if (compare != 0) return compare;
           if (m_size < s.m_size) return -1;
           if (m_size > s.m_size) return 1;
           return 0;
       }
       
+      int compare (size_type position, size_type count, basic_string_view stringw) const {return substr(position,count).compare(stringw);}
+
+      int compare (size_type position1, size_type count1, basic_string_view str, size_type position2, size_type count2) const
+      {
+         return substr(position1,count1).compare(str.substr(position2,count2));
+      }
+
+      int compare (const CharT* str) const {return compare(basic_string_view<CharT,Traits>(str));}
+      int compare (size_type position, size_type count, const CharT* str) const {return substr(position, count).compare(basic_string_view<CharT,Traits>(str));}
+      
+      int compare (size_type position, size_type count1, const CharT* str, size_type count2) const
+      {
+          return substr(position, count1).compare(basic_string_view<CharT,Traits>(str, count2));
+      }
+
+      template <typename tAllocator>
+      int compare (const std::basic_string<CharT,Traits,tAllocator>& str) const {return compare(basic_string_view<CharT,Traits>(str));}
+
+
+      /* Iterators */
+      const_iterator begin () const noexcept {return m_string;}
+      const_iterator cbegin() const noexcept {return begin();}
+      const_iterator end   () const noexcept {return m_string + m_size;}
+      const_iterator cend  () const noexcept {return end();}
+
+      /* Reverse iterators */
+      const_reverse_iterator rbegin () const noexcept {const_reverse_iterator{end()};}
+      const_reverse_iterator crbegin() const noexcept {return rbegin();}
+      const_reverse_iterator rend   () const noexcept {const_reverse_iterator{begin()};}
+      const_reverse_iterator crend  () const noexcept {return rend();}
+
+
       template <typename tAllocator>
       explicit constexpr operator std::basic_string<CharT, Traits, tAllocator>() const;
 
@@ -109,7 +150,7 @@ namespace lututils_std
   template <typename CharT, typename Traits>
   inline void swap (basic_string_view<CharT,Traits>& l, basic_string_view<CharT,Traits>& r) noexcept {l.swap(r);}
 
-  /* stream output operator */
+  /* output stream output operator */
   template <typename CharT, typename Traits>
   inline std::basic_ostream<CharT,Traits>& operator << (std::basic_ostream<CharT,Traits>& o, const basic_string_view<CharT,Traits>& s)
   { o.write(s.m_string(),static_cast<std::streamsize>(s.m_size())); return o;}
@@ -219,10 +260,10 @@ namespace lututils_std
   using u16string_view = lututils_std::basic_string_view<char16_t>;
   using u32string_view = lututils_std::basic_string_view<char32_t>;
 
-  constexpr string_view    operator ""_sv( const char*     str, std::size_t len ) noexcept {return string_view    {str, len};}
-  constexpr wstring_view   operator ""_sv( const wchar_t*  str, std::size_t len ) noexcept {return wstring_view   {str, len};}
-  constexpr u16string_view operator ""_sv( const char16_t* str, std::size_t len ) noexcept {return u16string_view {str, len};}
-  constexpr u32string_view operator ""_sv( const char32_t* str, std::size_t len ) noexcept {return u32string_view {str, len};}
+  constexpr string_view    operator ""sv( const char*     str, std::size_t len ) noexcept {return string_view    {str, len};}
+  constexpr wstring_view   operator ""sv( const wchar_t*  str, std::size_t len ) noexcept {return wstring_view   {str, len};}
+  constexpr u16string_view operator ""sv( const char16_t* str, std::size_t len ) noexcept {return u16string_view {str, len};}
+  constexpr u32string_view operator ""sv( const char32_t* str, std::size_t len ) noexcept {return u32string_view {str, len};}
 
 
 #endif /*  __cplusplus >= 201703L  */ 
