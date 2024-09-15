3715338a06e2a3e615c569160fea3f58c7664e47
MaratGMATCI <Marat.Shchuchinsky@gm.com>
2024-09-15 09:30:03 -0400

    tmp commit

--- LutUtils/include/HuffmanStream.h
+++ LutUtils/include/HuffmanStream.h
@@ -3,5 +3,18 @@
 
 #include <type_traits>
 
+class CHuffmanStream
+{
+public:
+
+ CHuffmanStream(){;}
+ CHuffmanStream(void* ptr) {;}
+ 
+ void* operator new(size_t s) {return nullptr;|
+ void  operator delete(void* p) {;}
+  
+private:
+
+};
 	
 #endif /* __LUT_LIBRARY_HUFFMAN_ENCODE_STREAM__ */ 
