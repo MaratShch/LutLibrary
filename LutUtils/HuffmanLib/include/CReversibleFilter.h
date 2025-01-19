15fa183dd6e691946bd2762b09093cdf9d7b8332
Marat Shchuchinsky <maratshch@gmail.com>
2025-01-19 21:31:41 +0200

    move Reversible Filter for HALD LUT into separate file. Add empty template for do migration

---
+++ LutUtils/HuffmanLib/include/CReversibleFilter.h
@@ -0,0 +1,119 @@
+#ifndef __C_PNG_REVERSIBLE_FILTER_API__
+#define __C_PNG_REVERSIBLE_FILTER_API__
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+namespace HuffmanUtils
+{
+    enum class eFILTER_T
+    {
+        FILTER_NONE = 0,
+        FILTER_SUB,
+        FILTER_UP,
+        FILTER_AVERAGE,
+        FILTER_PAETH
+    } ;
+
+    template <typename T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+    bool filter_line_reconstruct
+    (
+        const std::vector<uint8_t>& decoded,
+              std::vector<T>& restored,
+        const int32_t& inLineBytesSize,
+        const int32_t& outLineSize,
+        const int32_t& lineIdx,
+        const int32_t& channels
+    ) 
+    {
+        constexpr int32_t in_out_factor = sizeof(T) / sizeof(uint8_t);
+        const int32_t inChannelIncrement = channels * in_out_factor;
+        const int32_t lineIn  = inLineBytesSize  * lineIdx;
+        const int32_t lineOut = outLineSize * lineIdx;
+        const eFILTER_T& filterT = static_cast<const eFILTER_T>(decoded[lineIn]);
+
+        int32_t i, j, o;
+
+        switch (filterT)
+        {
+            case eFILTER_T::FILTER_NONE:
+            {
+                for (i = lineIn + 1, o = lineOut; o < outLineSize; o += channels, i += inChannelIncrement)
+                {
+
+                }
+            }
+            break;
+
+            case eFILTER_T::FILTER_SUB:
+            {
+                for (i = lineIn + 1, o = lineOut; o < outLineSize; o += channels, i += inChannelIncrement)
+                {
+
+                }
+            }
+            break;
+
+            case eFILTER_T::FILTER_UP:
+            {
+                for (i = lineIn + 1, o = lineOut; o < outLineSize; o += channels, i += inChannelIncrement)
+                {
+
+                }
+            }
+            break;
+
+            case eFILTER_T::FILTER_AVERAGE:
+            {
+                for (i = lineIn + 1, o = lineOut; o < outLineSize; o += channels, i += inChannelIncrement)
+                {
+
+                }
+            }
+            break;
+
+            case eFILTER_T::FILTER_PAETH:
+            {
+                for (i = lineIn + 1, o = lineOut; o < outLineSize; o += channels, i += inChannelIncrement)
+                {
+
+                }
+            }
+            break;
+
+            default:
+                std::runtime_error("Invalid filter type");
+            break;
+
+        } // switch (filterT)
+
+        return true;
+    }
+	
+
+    template <typename T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+    const std::vector<T> filter_data_reconstruct
+    (
+        const std::vector<uint8_t>& decoded, // decoded data
+        const int32_t sizeX,                 // horizontal image size in pixels
+        const int32_t sizeY,                 // vertical image size in pixels
+        const int32_t bpp,                   // bits per pixels (8, 16, 24, 32, 48, 64)
+        const int32_t channels               // color channel (1-B/W, 3-RGB, 4-RGBA)    
+    )
+    {
+        std::vector<T> out_data{};
+        const int32_t colorChannelDept = bpp / channels;
+        const int32_t inSizeBytes = 1 + sizeX * channels * (colorChannelDept / 8);
+        const int32_t outSize = sizeX * channels;
+        bool bResult = true;
+
+        for (int32_t i = 0; i < sizeY && true == bResult; i++)
+            bResult = filter_line_reconstruct(decoded, out_data, inSizeBytes, outSize, i, channels);
+
+        return out_data;
+    }
+}; // namespace HuffmanUtils
+
+#endif // __C_PNG_REVERSIBLE_FILTER_API__
