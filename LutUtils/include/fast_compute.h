120ad4bf048ba55ea06f1eda2150c6be3499a4d2
Marat Shchuchinsky (Office) <maratshch@gmail.com>
2025-12-15 15:10:11 +0200

    add tiny utilities file for smart numerical computations

---
+++ LutUtils/include/fast_compute.h
@@ -0,0 +1,21 @@
+#include <cstdint>
+
+namespace FastCompute
+{
+	inline float Floor (float x) noexcept
+	{
+		constexpr float magic = 12582912.0f; // 2^23 * 1.5
+		float y = x + magic;
+		y -= magic;
+		return y - (y > x);
+	}
+	
+	inline double Floor (double x) noexcept
+	{
+		constexpr double magic = 6755399441055744.0; // 2^52 * 1.5
+		double y = x + magic;
+		y -= magic;
+		return y - (y > x);
+	}
+
+}
