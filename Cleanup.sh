46b7c04980fecffa460228fad14419d627061e29
Marat Shchuchinsky (Office) <maratshch@gmail.com>
2026-04-19 10:42:41 +0300

    dev.

--- Cleanup.sh
+++ Cleanup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 WORK_DIR=$PWD
+BUILD_FOLDER0=$WORK_DIR/build
 BUILD_FOLDER1=$WORK_DIR/build_fast
 BUILD_FOLDER2=$WORK_DIR/build_high
 INSTALL_FOLDER=$WORK_DIR/install
 
 sync
 
+if [ -d $BUILD_FOLDER0 ]; then
+  rm -fr $BUILD_FOLDER0
+fi
 if [ -d $BUILD_FOLDER1 ]; then
   rm -fr $BUILD_FOLDER1
 fi
